af687c3ea71b03a94fbba1a8939740282822873e
silasmas <silasjmas@gmail.com>
2022-04-12 16:46:42 +0200

    edit menu

--- resources/views/parties/menu.blade.php
+++ resources/views/parties/menu.blade.php
@@ -57,236 +57,20 @@
                     <div class="template-main-menu">
                         <nav id="dropdown">
                             <ul>
-                                <li>
-                                    <a href="#">Home</a>
-                                    <ul class="dropdown-menu-col-1">
-                                        <li><a href="index.html">Home 1</a></li>
-                                        <li><a href="index2.html">Home 2</a></li>
-                                        <li><a href="index3.html">Home 3</a></li>
-                                        <li><a href="index4.html">Home 4</a></li>
-                                    </ul>
+                                <li class="active">
+                                    <a href="#">Accueil</a>                                    
                                 </li>
                                 <li>
-                                    <a href="#">About</a>
-                                    <ul class="dropdown-menu-col-1">
-                                        <li><a href="about1.html">About 1</a></li>
-                                        <li><a href="about2.html">About 2</a></li>
-                                        <li><a href="about3.html">About 3</a></li>
-                                    </ul>
+                                    <a href="#">Apropo</a>                                    
                                 </li>                                        
                                 <li>
-                                    <a href="#">Departments</a>
-                                    <ul class="dropdown-menu-col-1">
-                                        <li>
-                                            <a href="departments1.html">Departments 1</a>
-                                        </li>
-                                        <li>
-                                            <a href="departments2.html">Departments 2</a>
-                                        </li>
-                                        <li>
-                                            <a href="departments3.html">Departments 3</a>
-                                        </li>
-                                        <li>
-                                            <a href="single-departments.html">Departments Details</a>
-                                        </li>
-                                    </ul>
+                                    <a href="#">Nos services</a>                                   
                                 </li>
                                 <li>
-                                    <a href="#">Doctors</a>
-                                    <ul class="dropdown-menu-col-1">
-                                        <li>
-                                            <a href="doctors1.html">Doctors 1</a>
-                                        </li>
-                                        <li>
-                                            <a href="doctors2.html">Doctors 2</a>
-                                        </li>
-                                        <li>
-                                            <a href="single-doctor.html">Doctors Details</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="possition-static hide-on-mobile-menu">
-                                    <a href="#">Pages</a>
-                                    <div class="template-mega-menu">
-                                        <div class="container">
-                                            <div class="row">
-                                                <div class="col-3">
-                                                    <div class="menu-ctg-title">Home</div>
-                                                    <ul class="sub-menu">
-                                                        <li>
-                                                            <a href="index.html">
-                                                                <i class="fas fa-home"></i>Home 1</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="index2.html">
-                                                                <i class="fas fa-home"></i>Home 2</a>
-                                                        </li>
-                                                    </ul>
-                                                    <div class="menu-ctg-title">About</div>
-                                                    <ul class="sub-menu">
-                                                        <li>
-                                                            <a href="about.html">
-                                                                <i class="fab fa-cloudversify"></i>About</a>
-                                                        </li>
-                                                    </ul>
-                                                    <div class="menu-ctg-title">Doctors</div>
-                                                    <ul class="sub-menu">
-                                                        <li>
-                                                            <a href="doctors1.html">
-                                                                <i class="fas fa-user-md"></i>Doctors 1</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <div class="col-3">
-                                                    <ul class="sub-menu">
-                                                        <li>
-                                                            <a href="doctors2.html">
-                                                                <i class="fas fa-user-md"></i>Doctors 2</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="single-doctor.html">
-                                                                <i class="fas fa-user-md"></i>Doctors Details</a>
-                                                        </li>
-                                                    </ul>
-                                                    <div class="menu-ctg-title">Departments</div>
-                                                    <ul class="sub-menu">
-                                                        <li>
-                                                            <a href="departments1.html">
-                                                                <i class="fas fa-hospital"></i>Department 1</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="departments2.html">
-                                                                <i class="fas fa-hospital"></i>Department 2</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="departments3.html">
-                                                                <i class="fas fa-hospital"></i>Department 3</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="single-departments.html">
-                                                                <i class="fas fa-hospital"></i>Department
-                                                                Details
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <div class="col-3">
-                                                    <div class="menu-ctg-title">Pages</div>
-                                                    <ul class="sub-menu">
-                                                        <li>
-                                                            <a href="gallery.html">
-                                                                <i class="fas fa-clone"></i>Gallery</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="appointment.html">
-                                                                <i class="far fa-calendar-check"></i>Appointment</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="price-table.html">
-                                                                <i class="far fa-money-bill-alt"></i>Price
-                                                                Table
-                                                            </a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="shop.html">
-                                                                <i class="fas fa-shopping-basket"></i>Shop</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="single-shop.html">
-                                                                <i class="fas fa-shopping-basket"></i>Shop
-                                                                Details
-                                                            </a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="contact.html">
-                                                                <i class="fas fa-envelope"></i>Contact</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <div class="col-3">
-                                                    <ul class="sub-menu">
-                                                        <li>
-                                                            <a href="faq.html">
-                                                                <i class="fas fa-file-archive"></i>Faq List</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="404.html">
-                                                                <i class="fas fa-exclamation-triangle"></i>404
-                                                                Error
-                                                            </a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="coming-soon.html">
-                                                                <i class="fas fa-sort-amount-up"></i>Coming
-                                                                Soon
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                    <div class="menu-ctg-title">News</div>
-                                                    <ul class="sub-menu">
-                                                        <li>
-                                                            <a href="news1.html">
-                                                                <i class="far fa-newspaper"></i>News 1</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="news2.html">
-                                                                <i class="far fa-newspaper"></i>News 2</a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="single-news.html">
-                                                                <i class="far fa-newspaper"></i>News Details</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="hide-on-desktop-menu">
-                                    <a href="#">Pages</a>
-                                    <ul>
-                                        <li>
-                                            <a href="gallery.html">Gallery</a>
-                                        </li>
-                                        <li>
-                                            <a href="appointment.html">Appointment</a>
-                                        </li>
-                                        <li>
-                                            <a href="price-table.html">Price Table</a>
-                                        </li>
-                                        <li>
-                                            <a href="shop.html">Shop</a>
-                                        </li>
-                                        <li>
-                                            <a href="single-shop.html">Shop Details</a>
-                                        </li>
-                                        <li>
-                                            <a href="faq.html">Faq List</a>
-                                        </li>
-                                        <li>
-                                            <a href="404.html">404 Error</a>
-                                        </li>
-                                        <li>
-                                            <a href="coming-soon.html">Coming Soon</a>
-                                        </li>
-                                    </ul>
+                                    <a href="#">Nos docteurs</a>
                                 </li>
                                 <li>
-                                    <a href="#">News</a>
-                                    <ul class="dropdown-menu-col-1">
-                                        <li>
-                                            <a href="news1.html">News 1</a>
-                                        </li>
-                                        <li>
-                                            <a href="news2.html">News 2</a>
-                                        </li>
-                                        <li>
-                                            <a href="single-news.html">News Details</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="contact.html">Contact</a>
+                                    <a href="">Contact</a>
                                 </li>
                             </ul>
                         </nav>
@@ -304,7 +88,7 @@
                                 </form>
                             </li>
                             <li>
-                                <a href="appointment.html" class="action-items-primary-btn">Appointment<i class="fas fa-chevron-right"></i></a>
+                                <a href="" class="action-items-primary-btn">Prendre un RDV<i class="fas fa-chevron-right"></i></a>
                             </li>
                         </ul>
                     </div>
