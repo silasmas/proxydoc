f1a97f7618229ebb3f9bb89119f25cdf18ffee70
silasmas <silasjmas@gmail.com>
2022-04-13 04:33:10 +0200

    logo

--- resources/views/parties/entete.blade.php
+++ resources/views/parties/entete.blade.php
@@ -12,7 +12,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/logo/logo1.png') }}">
     <!-- Normalize CSS -->
     <link rel="stylesheet" href="{{ asset('assets/css/normalize.css') }}">
     <!-- Main CSS -->
